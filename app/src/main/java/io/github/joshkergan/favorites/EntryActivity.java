9ebbe1972d24eb170985ed492e3f61c3b7ef94b7
Josh Kergan <joshkergan@gmail.com>
2015-11-24 23:33:03 -0500

    Added Google Analytics to the app startup. Also removed a suppressed warning as a reminder to get to work on it.

--- app/src/main/java/io/github/joshkergan/favorites/EntryActivity.java
+++ app/src/main/java/io/github/joshkergan/favorites/EntryActivity.java
@@ -2,34 +2,52 @@ package io.github.joshkergan.favorites;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
 
 public class EntryActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+	private static final String TAG = "EntryActivity";
+
+	private Tracker mTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_entry);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+	    // Obtain the tracker instance
+	    AnalyticsApplication application = (AnalyticsApplication) getApplication();
+	    mTracker = application.getDefaultTracker();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+	    fab.setOnClickListener(new View.OnClickListener(){
+		    @Override
+		    public void onClick(View view) {
+			    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+					    .setAction("Action", null).show();
+
+			    // Analytics call for clicking the fab
+			    mTracker.send(new HitBuilders.EventBuilder()
+					    .setCategory("Action")
+					    .setAction("Fab_Open")
+					    .build());
+		    }
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -40,6 +58,12 @@ public class EntryActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+	    String name = "Main";
+
+	    Log.i(TAG, "Setting screen name: " + name);
+	    mTracker.setScreenName(name);
+	    mTracker.send(new HitBuilders.ScreenViewBuilder().build());
     }
 
     @Override
@@ -74,7 +98,6 @@ public class EntryActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
